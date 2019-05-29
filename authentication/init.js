cbfd76297b319a18d0ab38e19c589c7b4fcacd06
kozelArtem <kozel.as@yandex.ru>
2019-05-29 15:14:16 +0300

    removed unused dependencies

--- authentication/init.js
+++ authentication/init.js
@@ -1,7 +1,6 @@
 const passport = require('passport');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const session = require('express-session');
 const config = require('../config/passport.json');
 
 
@@ -12,10 +11,7 @@ module.exports = (app) => {
     app.use(bodyParser.urlencoded({
         extended: false
     }));
-    app.use(session(config.session));
 
     // Passport:
     app.use(passport.initialize());
-    app.use(passport.session());
-
 }
